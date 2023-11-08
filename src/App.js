8671f042d1643e8fe895a436370f0c0500677e49
albajosephmarie <albajosephmarie@gmail.com>
2023-11-09 00:00:42 +0800

    Sign-in Sign-out Forgot Password OAuth

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
-import SignOut from "./pages/SignOut";
+import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
 import Header from "./components/Header";
@@ -16,7 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-out" element={<SignOut />} />
+          <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/offers" element={<Offers />} />
         </Routes>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,9 +3,9 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import OAuth from "../components/OAuth";
 
-export default function SignIn() {
-  const [formData, setFormData] = useState({ email: "", password: "" });
-  const { email, password } = formData;
+export default function SignUp() {
+  const [formData, setFormData] = useState({ name:"", email: "", password: "" });
+  const { name, email, password } = formData;
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
@@ -14,7 +14,7 @@ export default function SignIn() {
   };
   return (
     <section>
-      <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
+      <h1 className="text-3xl text-center mt-6 font-bold">Sign Up</h1>
       <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl max-auto">
         <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
           <img
@@ -25,6 +25,14 @@ export default function SignIn() {
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
           <form className="">
+            <input
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+              type="text"
+              id="name"
+              value={name}
+              onChange={onChange}
+              placeholder="Full name"
+            />
             <input
               className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
               type="email"
@@ -45,12 +53,12 @@ export default function SignIn() {
             </div>
             <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
               <p className="mb-6">
-                Don't have an account?{" "}
+                Have an account?{" "}
                 <Link
-                  to="/sign-up"
+                  to="/sign-in"
                   className="text-red-600 hover:text-red-800 transition duration-200 ease-in-out ml-1"
                 >
-                  Register
+                  Sign in
                 </Link>
               </p>
               <p>
@@ -66,7 +74,7 @@ export default function SignIn() {
               className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
               type="submit"
             >
-              Sign in
+              Sign up
             </button>
             <div className="my-4 before:border-t flex before:flex-1 items-center before:border-gray-300 after:border-t after:flex-1 after:border-gray-300">
               <p className="text-center font-semibold mx-4">OR</p>
